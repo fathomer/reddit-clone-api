bbb5fbcf01025f148f2ae9510341004c7d067649
fathomer <kartikabc712@gmail.com>
2021-04-04 23:51:51 +0530

    Boilerplate Project Setup

---
+++ src/main/java/com/reddit/clone/LikeRedditApplication.java
@@ -0,0 +1,13 @@
+package com.reddit.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likereddit/LikeRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likereddit;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
